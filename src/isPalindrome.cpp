380de4d3fc3bc004c162aab40694cec2eb381001
Roman <rpilyushin@gmail.com>
2024-08-30 18:11:08 +0300

    Add 125. Valid Palindrome

--- src/isPalindrome.cpp
+++ src/isPalindrome.cpp
@@ -1,7 +1,31 @@
 #include "isPalindrome.h"
 #include <string>
 
-bool isPalindrome::isPal(std::string s) {
+bool c::isPal(std::string s) {
 
-    return true;
+    int left = 0;
+    int right = s.length() - 1;
+
+    while (left < right) {
+        //Move left pointer to the right while it's not pointing to an alphanumeric character
+        while (left < right && !std::isalnum(s[left])) {
+            left++;
+        }
+
+        // Move right pointer to the left while it's not pointing to an alphanumeric character
+        while (left < right && !std::isalnum(s[right])) {
+            right--;
+        }
+
+        // Compare the characters at left and right pointers after converting them to lowercase
+        if (std::tolower(s[left]) != std::tolower(s[right])) {
+            return false; // Not a palindrome
+        }
+
+        // Move both pointers towards the center
+        left++;
+        right--;
+    }
+
+    return true; // It's a palindrome
 }
